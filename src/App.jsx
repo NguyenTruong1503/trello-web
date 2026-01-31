5a56ef38544263b4b41591a242dfd6b42bc9f4e3
truongnguyen <ndt15032003@gmail.com>
2026-01-31 22:53:28 +0700

    A A Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,12 @@
 import React from 'react'
-import {useColorScheme} from "@mui/material/styles";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Box from '@mui/material/Box';
-import Container from "@mui/material/Container";
-import theme from "./theme.js";
+import Board from "./pages/Boards/_id.jsx";
 
-
-
-function ModeSelect() {
-    const { mode, setMode} = useColorScheme();
-
-    const handleChange = (event) => {
-        const selectMode = event.target.value;
-        setMode(selectMode);
-    };
-
-    return (
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-            <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-            <Select
-                labelId="label-select-dark-light-mode"
-                id="select-dark-light-mode"
-                value={mode}
-                label="Mode"
-                onChange={handleChange}
-            >
-                <MenuItem value='light'>
-                    <div style={{display: 'flex' , alignItems: 'center', gap: '10px'}}>
-                        <LightModeIcon fontSize="small"/>
-                        Light
-                    </div>
-                </MenuItem>
-                <MenuItem value='dark'>
-                    <div style={{display: 'flex' , alignItems: 'center', gap: '10px'}}>
-                        <DarkModeOutlinedIcon />
-                        Dark
-                    </div>
-                </MenuItem>
-                <MenuItem value='system'>
-                    <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}} >
-                        <SettingsBrightnessIcon />
-                        System
-                    </Box>
-                </MenuItem>
-            </Select>
-        </FormControl>
-    );
-}
-
+// Se lam react router dom
 const App = () => {
     return (
-        <Container disableGutters maxWidth={false} sx={{height: '100vh'}}>
-            <Box sx={{backgroundColor: 'primary.light', height: (theme) => theme.trello.appBarHeight, width: '100%', display: 'flex', alignItems: 'center'}}>
-                <ModeSelect/>
-            </Box>
-            <Box sx={{backgroundColor: 'primary.dark', height: (theme) => theme.trello.boardBarHeight, width: '100%', display: 'flex', alignItems: 'center'}}>Board Bar</Box>
-            <Box sx={{backgroundColor: 'primary.main', height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`, width: '100%', display: 'flex', alignItems: 'center'}}>Board Content</Box>
-        </Container>
+        <>
+            <Board/>
+        </>
     )
 }
 export default App
---
+++ src/pages/Boards/BoardContent/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Box from "@mui/material/Box";
+
+const BoardContent = () => {
+    return (
+        <Box sx={{backgroundColor: 'primary.main', height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`, width: '100%', display: 'flex', alignItems: 'center'}}>Board Content</Box>
+    )
+}
+export default BoardContent
